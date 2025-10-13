a5b2d3b2c2feaa9635aa4d667ee2f752a047187d
oficialThur <oficialthurhenrique@gmail.com>
2025-10-13 19:58:11 -0300

    aula5/6/7

---
+++ aula5/scripts.js
@@ -0,0 +1,16 @@
+// declarar uma variavel sem valor.
+
+let user
+
+console.log("Arthur")
+
+// declarar variavel com valor
+
+let email = "devthur@email.com"
+
+console.log(email)
+
+email = "devthur2@gmail.com"
+
+console.log(email)
+
---
+++ aula6/scripts.js
@@ -0,0 +1,7 @@
+// uma constante cria uma variavel cujo o valor e fixo
+
+const number = 22
+
+console.log(number)
+
+
